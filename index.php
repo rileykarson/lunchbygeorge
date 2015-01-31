3be4e5a02d2eca87acc24cc2939c6454db99cbe5
Riley Karson <rileykarson@test.com>
2015-01-31 12:15:13 -0500

    Added text content

--- index.php
+++ index.php
@@ -19,10 +19,58 @@
 		include "navbar.html"
 	?>
 		<div class= "row">
-			<div "columns small-12">
+			<div class = "columns small-12">
 				<img src = "./img/header.jpg" alt="Lunch By George">
 			</div>
-		</dow>
+		</div>
+		<div class = "row">
+			<div class = "columns small-12">
+				<h1>Demand Continues</h1>
+				<p>
+					The number of clients we served showed a slight decline in 2012, but not enough to suggest that there is no longer a need for our services. we served 8790 soups (a 1% decline) and 11307 meals (a 5% decline). Our January figures are also a little lower, but it was still the second highest January demand for food in the 26 years for which we have records. There has been an increased demand for clothing and our clients were especially grateful for a wonderful donation of winter apparel from the Cunningham Swan law firm 
+				</p>
+			</div>
+		</div>
+		<div class = "row">
+			<div class = "columns small-12">
+				<h1>Honorary Patron</h1>
+				<p>
+					We are pleased to announce that The Honourable Hugh D Segal, C.M. has accepted the position of Honorary Patron of Lunch By George. Senator Segal is a long time champion of the cause of the poor, the alienated and the disenfranchised, and we are delighted to have his support.
+				</p>
+			</div>
+		</div>
+		<div class = "row">
+			<div class = "columns small-12">
+				<h1>Annual General Meeting</h1>
+				<p>
+					Our Annual General Meeting was held on Monday July 15th at in Lunch By George, 129 Wellington Street, Kingston. Those in attendance approved the Board decisions of 2012, and the audited financial statements. They also approved various proposals for activities in the coming year.
+				</p>
+				</div>
+		</div>
+		<div class = "row">
+			<div class = "columns small-12">
+				<h1>Fundraising Dinner</h1>
+				<p>
+					Thanks to all who came and bid in the silent auction, or bought mugs. We raised $2700 on Friday and all enjoyed ourselves. It is never too late to contribute if you were busy or forgot!
+				</p>
+				</div>
+		</div>
+		<div class = "row">
+			<div class = "columns small-12">
+				<h1>Anglican Churches in Ontario Diocese </h1>
+				<p>
+					have again shown their generosity by donating $1802 from their special ordination service in May. Thank you all for thinking of the needy.
+				</p>
+				</div>
+		</div>
+		<div class = "row">
+			<div class = "columns small-12">
+				<h1>New Major Donors</h1>
+				<p>
+					We are delighted to be the recipients of grants from the Cataraqui-Kingston Rotary Club, and the Queen's MCRC Fines Revenue Allocation Committee. With the extra money being spent on food this year, their contributions are most welcome!
+				</p>
+				</div>
+		</div>
 
 	 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script src="js/plugins.js"></script>
