54187bc2d44ea643da249120159e085d198a3376
Riley Karson <rileykarson@test.com>
2015-01-31 12:50:17 -0500

    Changed index page to be generated from PHP

---
+++ strings.php
@@ -0,0 +1,12 @@
+<?php
+	$array = array(
+		array(
+        	"header" => "Demand Continues",
+        	"content" => "The number of clients we served showed a slight decline in 2012, but not enough to suggest that there is no longer a need for our services. we served 8790 soups (a 1% decline) and 11307 meals (a 5% decline). Our January figures are also a little lower, but it was still the second highest January demand for food in the 26 years for which we have records. There has been an increased demand for clothing and our clients were especially grateful for a wonderful donation of winter apparel from the Cunningham Swan law firm. "
+         ),
+		array(
+        	"header" => "Honorable Mentions",
+        	"content" => "We are pleased to announce that The Honourable Hugh D Segal, C.M. has accepted the position of Honorary Patron of Lunch By George. Senator Segal is a long time champion of the cause of the poor, the alienated and the disenfranchised, and we are delighted to have his support."
+         )
+	);
+?>
