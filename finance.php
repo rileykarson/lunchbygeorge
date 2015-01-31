752bf3538154390c8e60b27f34a2301290d60df4
alexandramcmanus <ally-mcmanus@hotmail.com>
2015-01-31 15:27:09 -0500

    Update finance.php
    added class="button"

--- finance.php
+++ finance.php
@@ -23,14 +23,14 @@
 			<h2>Financial Statements</h2>
 				<p>2013 <a href="/web/viewer.html?file=%2F2013statement.pdf"> Preview </a> <a href="./2013statement.pdf"> Download</a></p>
 			<h2>Audits</h2>
-			<p> Audit 1 <a href="/web/viewer.html?file=%2Faudit1.pdf"> class="button" Preview </a> <a href="./audit1.pdf"> Download </a> </p>
-			<p> Audit 2 <a href="/web/viewer.html?file=%2Faudit2.pdf"> class="button" Preview </a> <a href="./audit2.pdf"> Download </a> </p>
-			<p> Audit 3 <a href="/web/viewer.html?file=%2Faudit3.pdf"> class="button" Preview </a> <a href="./audit3.pdf"> Download </a> </p>
-			<p> Audit 4 <a href="/web/viewer.html?file=%2Faudit4.pdf"> class="button" Preview </a> <a href="./audit4.pdf"> Download </a> </p>
-			<p> Audit 5 <a href="/web/viewer.html?file=%2Faudit5.pdf"> class="button" Preview </a> <a href="./audit5.pdf"> Download </a> </p>
-			<p> Audit 6 <a href="/web/viewer.html?file=%2Faudit6.pdf"> class="button" Preview </a> <a href="./audit6.pdf"> Download </a> </p>
-			<p> Audit 7 <a href="/web/viewer.html?file=%2Faudit7.pdf"> class="button" Preview </a> <a href="./audit7.pdf"> Download </a> </p>
-			<p> Audit 8 <a href="/web/viewer.html?file=%2Faudit8.pdf"> class="button" Preview </a> <a href="./audit8.pdf"> Download </a> </p>
+			<p> Audit 1 <a href="/web/viewer.html?file=%2Faudit1.pdf" class="button">  Preview </a> <a href="./audit1.pdf" class="button"> Download </a> </p>
+			<p> Audit 2 <a href="/web/viewer.html?file=%2Faudit2.pdf" class="button">  Preview </a> <a href="./audit2.pdf" class="button"> Download </a> </p>
+			<p> Audit 3 <a href="/web/viewer.html?file=%2Faudit3.pdf" class="button">  Preview </a> <a href="./audit3.pdf" class="button"> Download </a> </p>
+			<p> Audit 4 <a href="/web/viewer.html?file=%2Faudit4.pdf" class="button">  Preview </a> <a href="./audit4.pdf" class="button"> Download </a> </p>
+			<p> Audit 5 <a href="/web/viewer.html?file=%2Faudit5.pdf" class="button">  Preview </a> <a href="./audit5.pdf" class="button"> Download </a> </p>
+			<p> Audit 6 <a href="/web/viewer.html?file=%2Faudit6.pdf" class="button">  Preview </a> <a href="./audit6.pdf" class="button"> Download </a> </p>
+			<p> Audit 7 <a href="/web/viewer.html?file=%2Faudit7.pdf" class="button">  Preview </a> <a href="./audit7.pdf" class="button"> Download </a> </p>
+			<p> Audit 8 <a href="/web/viewer.html?file=%2Faudit8.pdf" class="button">  Preview </a> <a href="./audit8.pdf" class="button"> Download </a> </p>
 		</div>
 	 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script src="js/plugins.js"></script>
