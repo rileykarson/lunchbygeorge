03c2e3bf001ebc91ca20e2a1829fd41245c2423b
Riley Karson <rileykarson@test.com>
2015-01-31 15:20:52 -0500

    Fixed strings.php

--- strings.php
+++ strings.php
@@ -19,29 +19,29 @@
                 array(
                 "header" => "Fresh Vegetables",
                 "content" => "In the depths of last winter, John Poland suggested that we should take a piece of land at the Lakeshore Community Garden and grow our own vegetables. We now have a plot 110 by 25 feet, and in 2014 this produced 1900 pounds of vegetables. There were weekly deliveries to our kitchen, and we still have much frozen, or in the Loving Spoonful bank. We look forward to repeating the garden’s success this year, and look forward to meeting volunteers who could help us with its cultivation."
-        )
+        ),
                 array(
-                "header" => "Annual General Meeting"
+                "header" => "Annual General Meeting",
                 "content" => "Our Annual General Meeting was held on Monday July 15th at in Lunch By George, 129 Wellington Street, Kingston. Those in attendance approved the Board decisions of 2012, and the audited financial statements. They also approved various proposals for activities in the coming year."
-        )
+        ),
                 array(
-                "header" => "Fundraising Dinner"
+                "header" => "Fundraising Dinner",
                 "content" => "Thanks to all who came and bid in the silent auction, or bought mugs. We raised $2700 on Friday and all enjoyed ourselves. It is never too late to contribute if you were busy or forgot!"
-        )
+        ),
                 array(
-                "header" => "Anglican Churches in Ontario Diocese"
+                "header" => "Anglican Churches in Ontario Diocese",
                 "content" => "have again shown their generosity by donating $1802 from their special ordination service in May. Thank you all for thinking of the needy."
-        )
+        ),
                 array(
-                "header" => "New Major Donors"
+                "header" => "New Major Donors",
                 "content" => "We are delighted to be the recipients of grants from the Cataraqui-Kingston Rotary Club, and the Queen's MCRC Fines Revenue Allocation Committee. With the extra money being spent on food this year, their contributions are most welcome!"
-        )
-                array(
+        ),
+                array(,
                 "header" => "Regina Rosen Food First Fund"
                 "content" => "We were delighted and thankful to be chosen as a recipient of a grant from this new fund. We were chosen by Sandy Singers because 'This meal program ... has been in operation for many years now and delivers a great service on a shoe-string budget. They are often overlooked with the emergence of newer programmes with higher profiles, yet they have been consistent in providing quality meals to our homeless and low-income neighbours, regardless of the attention'. Thank you for those kind words, and thank you, Regina Rosen, for creating the Fund. The money will be well used."
-        )
+        ),
                 array(
-                "header" => "Donations"
+                "header" => "Donations",
                 "content" => "We wish to thank all of you who remembered us  through all of 2013, with your generous donations. Remember it is never too early to start your 2014 donations! You are always welcome to drop by, any weekday morning before 11.15, to see how your money is spent."
         )
 	);
