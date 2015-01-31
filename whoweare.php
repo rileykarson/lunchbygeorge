67af3fb7d35d11d9a950d55bfe4fa24dfc5e2361
Riley Karson <rileykarson@test.com>
2015-01-31 13:58:16 -0500

    Syncing

---
+++ whoweare.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Lunch By George</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href = "//cdn.foundation5.zurb.com/foundation.css" rel="stylesheet">
+		<link href = "css/index.css" rel="stylesheet">
+		<link rel="stylesheet" src="//normalize-css.googlecode.com/svn/trunk/normalize.css" />
+		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<?php
+			include "navbar.html"
+		?>
+		<div class = "row">
+			<div class = "columns small-12 medium-6">
+				<h1>When We Are Open</h1>
+					<p>
+						Monday to Friday 9:00-11:30am
+						9 a.m. and onwards: Coffee and goodies
+						10 a.m. Soup
+						11 a.m. Hot Nutritious Meal
+					</p>
+			</div>
+			<div class = "columns small-12 medium-6">
+				<img src = "./img/infoimage.jpg">
+			</div>
+		</div>
+		<div class = "row">
+			<div class = "columns small-12 medium-6">
+				<h1>Board of Directors</h1>
+				<p>
+					Co-Chairs: Aveleigh Kyle and Peter Gower
+					Secretary: Tanis Fairley
+					Treasurer: Don Large
+					Members: Juanita Barrett, George Boland, Theo Bruce, Gill Cleghorn, David Gordon, Jim McCartney and Linda Morgan.
+				</p>
+			</div>
+			<div class = "columns small-12 medium-6">
+				<h1>How to contact us:</h1>
+				<p>
+					Lunch By George
+					129 Wellington Street
+					P.O. Box 475
+					Kingston, Ontario, K7L 4W5
+
+					Telephone: 613 548 4617 ext. 30
+
+					Fax: 613 548 7466
+
+					Email: info@osgk.org
+				</p>
+			</div>
+		</div>
+		<div class = "row">
+			<div class = "columns small-12 medium-6">
+				<img src = "./img/infoimage.jpg">
+			</div>
+			<div class = "columns small-12 medium-6">
+				<h1>Where?</h1>
+				<p>
+					St. George's Hall
+					129 Wellington St.
+					(corner of Johnson)
+					Lunch By George
+					129 Wellington Street
+					P.O. Box 475
+					Kingston, Ontario, K7L 4W5
+    			</p>
+			</div>
+		</div>
+		
+
+	 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/main.js"></script>
+		<script src="js/index.js"></script>
+	</body>
+</html>
