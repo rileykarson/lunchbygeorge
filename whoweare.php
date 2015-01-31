e80d3d116fc109da283d845713ffd09111a521e3
Riley Karson <rileykarson@test.com>
2015-01-31 15:14:01 -0500

    Changed bad file names

--- whoweare.php
+++ whoweare.php
@@ -20,63 +20,25 @@
 		?>
 		<div class = "row">
 			<div class = "columns small-12 medium-6">
-				<h1>When We Are Open</h1>
-					<p>
-						Monday to Friday 9:00-11:30am
-						9 a.m. and onwards: Coffee and goodies
-						10 a.m. Soup
-						11 a.m. Hot Nutritious Meal
-					</p>
-			</div>
-			<div class = "columns small-12 medium-6">
-				<img src = "./img/infoimage.jpg">
-			</div>
-		</div>
-		<div class = "row">
-			<div class = "columns small-12 medium-6">
-				<h1>Board of Directors</h1>
-				<p>
-					Co-Chairs: Aveleigh Kyle and Peter Gower
-					Secretary: Tanis Fairley
-					Treasurer: Don Large
-					Members: Juanita Barrett, George Boland, Theo Bruce, Gill Cleghorn, David Gordon, Jim McCartney and Linda Morgan.
+				<h1>Lunch by George</h1>
+				<p>Lunch by George normally serves 40-50 people per day, with an occasional 'spike' to the 60-70 range and over 100 at our Christmas meal. The program operates five days a week, opening at 9:00 a.m. with coffee and snacks and serving soup at 10:00 a.m. and a hot nutritious lunch at 11:00 a.m.. Normally the dishes are done and the kitchen cleaned up before noon, which leaves the hall available for other groups in the afternoon.
+				</p><p>Many of the patrons have some form of disability - mental or physical - which prevents them from working. Many of these patrons have been coming for a number of years. Lunch by George is their social centre; they know each other and care for each other. A few are elderly and in poor circumstances.
+				</p><p>A survey done several years ago showed that most were on disability, pension or welfare assistance, with rents consuming 30 to 50% of their incomes. Many have no cooking facilities - maybe just a hotplate - and limited opportunity to cook in a functional kitchen. Yet other patrons are only temporary: they are those who are employable but are having difficulty finding work. They use our services until their status improves or they move to another community with better employment prospects.
+				</p><p>Over the last two or three years the numbers attending the Lunch Program have been gradually increasing - due to cutbacks in support programs for the poor and fewer employment opportunities. We are glad that we can keep the kitchen open five days a week, fifty-one weeks of the year, to help support folks in need. 
 				</p>
 			</div>
 			<div class = "columns small-12 medium-6">
-				<h1>How to contact us:</h1>
-				<p>
-					Lunch By George
-					129 Wellington Street
-					P.O. Box 475
-					Kingston, Ontario, K7L 4W5
-
-					Telephone: 613 548 4617 ext. 30
-
-					Fax: 613 548 7466
-
-					Email: info@osgk.org
-				</p>
+				<div class = "imageHolder">
+					<img src = "./img/infoimage.jpg">
+				</div>
+				<div class = "imageHolder">
+					<img src = "./img/infoimage.jpg">
+				</div>
+				<div class = "imageHolder">
+					<img src = "./img/infoimage.jpg">
+				</div>
 			</div>
 		</div>
-		<div class = "row">
-			<div class = "columns small-12 medium-6">
-				<img src = "./img/infoimage.jpg">
-			</div>
-			<div class = "columns small-12 medium-6">
-				<h1>Where?</h1>
-				<p>
-					St. George's Hall
-					129 Wellington St.
-					(corner of Johnson)
-					Lunch By George
-					129 Wellington Street
-					P.O. Box 475
-					Kingston, Ontario, K7L 4W5
-    			</p>
-			</div>
-		</div>
-		
-
 	 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script src="js/plugins.js"></script>
 		<script src="js/main.js"></script>
