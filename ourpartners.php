5ba1d72a431bdb07fcee47af686b7bbf86eadfab
Raymond Chung <raymond_chung2@hotmail.com>
2015-01-31 18:21:39 -0500

    conflicts nooo

---
+++ ourpartners.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Lunch By George</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width<li> initial-scale=1">
+		<link href = "//cdn.foundation5.zurb.com/foundation.css" rel="stylesheet">
+		<link href = "css/index.css" rel="stylesheet">
+		<link rel="stylesheet" src="//normalize-css.googlecode.com/svn/trunk/normalize.css" />
+		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<?php
+			include "navbar.html"
+		?>
+		
+  <div class="partners">
+  <h2><b>Special 2012 Thanks:</b></h2>
+<p>We are pleased to announce that we have recently added two major donors to our ranks: The Rotary Club of Cataraqui-Kingston and the Newcomers Club of Kingston have both made us recipients of donations, and we sincerely thank them for their help.
+We also wish to those who contribute food on an ongoing basis:</p>
+<li>Beethoven Run 
+<li>Canadian Tire Cataraqui
+<li>Chit Chat Café
+<li>Christ Church Belleville
+<li>Cora’s
+<li>Food Not Bombs
+<li>Frontenac Mental Health
+<li>Holy Cross HS
+<li>Kingston Curling Club
+<li>KFC
+<li>Loving Spoonful
+<li>Partners in Mission Food Bank
+<li>Queen's Health Sciences
+<li>Raging Bull
+<li>Ruffled Feather
+<li>St. George’s
+<li>St. Luke's
+<li>St. Mary's Newburgh
+<li>St. Peter's
+<li>St. Thomas
+<li>The Shriners Club
+<li>Starbucks and Taoist Tai Chi.
+  </div>
+
+	 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/main.js"></script>
+		<script src="js/index.js"></script>
+	</body>
+</html>
